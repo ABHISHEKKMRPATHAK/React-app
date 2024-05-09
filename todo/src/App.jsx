51d2a027279f1b31500dbda2043b37a79dae5bba
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-05-09 20:21:50 +0530

    up

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {ToDoProvider } from './contexts/index'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [todos,setTodos] = useState([])
 
+ const addToDo = (todo) =>{
+  setTodos((prev)=>[{id:Date.now(), ...todo},...prev])
+ }
+
+ const updateToDo = (id,todo)=>{
+  setTodos((prev)=>prev.map((prevTodo) => (prevTodo.id == id ? todo : prevTodo)))
+ }
+
+ const deleteToDo = (id) => {
+  setTodos((prev) => prev.filter((todo)=>todo.id != id) )
+ }
+
+ const toggleComplete = (id) => {
+  setTodos((prev) => prev.map((prevTodo)=>prevTodo===id ?{...prevTodo,completed:!prevTodo.completed}: prevTodo))
+ }
   return (
-    <>
-     <h1>Hello</h1>
-    </>
+    <ToDoProvider value={{todos,addToDo,deleteToDo,updateToDo,toggleComplete}}>
+    <div className="bg-[#172842] min-h-screen py-8">
+    <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+        <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+        <div className="mb-4">
+            {/* Todo form goes here */} 
+        </div>
+        <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+        </div>
+    </div>
+</div>
+</ToDoProvider>
   )
 }
 
--- todo/src/contexts/ToDoContext.js
+++ todo/src/contexts/ToDoContext.js
@@ -1,7 +1,21 @@
-import { createContext,useContext } from "react";
+import { createContext, useContext } from "react";
 
-export  const ToDoContext  = createContext({});
+export const ToDoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Lern React",
+      completed: "false",
+    },
+  ],
+  addToDo: (todo) => {},
+  deleteToDo: (id) => {},
+  updateToDo: (id, todo) => {},
+  toggleComplete: (id) => {},
+});
 
-export const ToDo = () =>{
-    useContext(ToDoContext)
-}
+export const useTodo = () => {
+  useContext(ToDoContext);
+};
+
+export const ToDoProvider = ToDoContext.Provider;
