54b9d677b0f4342ac003d792f5e65895a7f6c2e5
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-07-30 15:44:34 +0530

    up

---
+++ meso/src/App.js
@@ -0,0 +1,10 @@
+import Home from '/Users/abhishekkumarpathak/React-app/meso/src/pages/homepage/Home.js'
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ meso/src/components/topbar/TopBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./topbar.css"
+
+function TopBar() {
+  return (
+    <div className='topbar-container'>
+     <div className="topBar-left"></div>
+     <div className="topbar-right"></div>
+     <div className="topbar-center"></div>
+
+    </div>
+  )
+}
+
+export default TopBar
