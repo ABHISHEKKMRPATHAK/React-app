3d74ebfbb9694fde607cbd51751fcc5ff61a5a02
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-08-03 15:04:53 +0530

    up

--- meso/src/components/topbar/TopBar.jsx
+++ meso/src/components/topbar/TopBar.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import "./topbar.css"
+import SearchIcon from '@mui/icons-material/Search';
 
 function TopBar() {
   return (
     <div className='topbar-container'>
-     <div className="topBar-left"></div>
-     <div className="topbar-right"></div>
+     <div className="topBar-left">
+      <span className='logo'>Social</span>
+     </div>
+     <div className="topbar-right">
+
+     </div>
      <div className="topbar-center"></div>
-     <div className="topbar-center"></div>
+    <div className='search-bar'> 
+      <SearchIcon/>
+      <input placeholder='Search Name'/>
+    </div>
     </div>
   )
 }
