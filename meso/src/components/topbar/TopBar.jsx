748f6c5583f00b21543f0ace5e47a78a84f54093
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-08-08 16:10:23 +0530

    up

--- meso/src/components/topbar/TopBar.jsx
+++ meso/src/components/topbar/TopBar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./topbar.css"
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from '@mui/icons-material/Person';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ChatIcon from '@mui/icons-material/Chat'
 
 function TopBar() {
   return (
@@ -8,13 +11,38 @@ function TopBar() {
      <div className="topBar-left">
       <span className='logo'>Social</span>
      </div>
-     <div className="topbar-right">
-
-     </div>
      <div className="topbar-center"></div>
     <div className='search-bar'> 
       <SearchIcon/>
-      <input placeholder='Search Name'/>
+      <input placeholder='Search Name' className='search-input'/>
+    </div>
+
+    <div className="topbar-right">
+       <div className="topbar-links">
+         <span className="topbar-link">HomePage</span>
+           <span className="topbar-link">Timeline</span>
+       </div>
+
+<div className="topbar-Icons">
+  <div className="topbar-icon-items">
+     <PersonIcon/>
+      <span className="topbar-icon-badge">1</span>
+  </div>
+</div>
+
+<div className="topbar-Icons">
+  <div className="topbar-icon-items">
+     <NotificationsIcon/>
+      <span className="topbar-icon-badge">3</span>
+  </div>
+</div>
+
+<div className="topbar-Icons">
+  <div className="topbar-icon-items">
+     <ChatIcon/>
+      <span className="topbar-icon-badge">7</span>
+  </div>
+</div>
     </div>
     </div>
   )
