e066fad45f9306b02b428c78c52da5e5aa0a6cab
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-09-07 23:14:04 +0530

    up

---
+++ EatIn/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from './pages/navbar/Navbar'
+
+function App() {
+
+  return (
+   <div className='app'>
+    <div className='navbar'>
+      <Navbar/>
+    </div>
+   </div>
+  )
+}
+
+export default App
---
+++ EatIn/src/pages/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={assets.logo}></img>
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile App</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
